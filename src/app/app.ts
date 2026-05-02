b76ead4b5f6ed0ccba7c8f75db8bcaf51e09f7ac
Harshada Keste <kesteharshada87@gmail.com>
2026-05-02 19:37:32 +0530

    angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('random-name-picker');
+export class AppComponent {
+
+  newName: string = "";
+  names: string[] = [];
+  selectedName: string = "";
+
+  addName() {
+    if (this.newName.trim() !== "") {
+      this.names.push(this.newName);
+      this.newName = "";
+    }
+  }
+
+  pickRandom() {
+    if (this.names.length === 0) {
+      alert("Add some names first!");
+      return;
+    }
+
+    const index = Math.floor(Math.random() * this.names.length);
+    this.selectedName = this.names[index];
+  }
 }
